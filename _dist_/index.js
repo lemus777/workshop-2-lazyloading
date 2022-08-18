587d5636c58c5ac495ff5f8cb38981347b665f7d
lemus777 <lemus777@users.noreply.github.com>
2022-08-18 18:53:27 +0000

    deploy: 7a163a9050085eeb3e395da35b79855f16f0b952

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,6 +1,39 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { registerImage } from "./lazy.js";
 
-console.log('Happy hacking :)')
+
+const mountNode = document.getElementById('images');
+const addButton = document.getElementById('addButton');
+
+const max = 123;
+const min = 1;
+const random = () => Math.floor(Math.random() * (max - min) + min);
+
+const createImageNode = () => {
+    const container = document.createElement('div');
+    container.className = 'p-4';
+
+    const imagen = document.createElement('img');
+    imagen.className = 'mx-auto';
+    imagen.width = '320';
+    imagen.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+    container.appendChild(imagen);
+
+    return container;
+};
+
+const nuevaImagen = createImageNode();
+
+const addImage = () => {
+    const newImage = createImageNode(); // aqui si lleva paréntesis
+    mountNode.appendChild(newImage);
+    registerImage(newImage); // esto usa la funcion importada para ver si está visible la imagen
+}
+
+addButton.addEventListener('click', addImage); // aqui el addImage va sin el paréntesis
+
+// por qué al llamar funciones hay veces que lleva paréntesis y otras que no?
+
+
+
+
