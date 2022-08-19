c4b139bd7d8d35d5ad728eb2c1dd9416c1365e51
lemus777 <lemus777@users.noreply.github.com>
2022-08-19 10:29:42 +0000

    deploy: 4d0f8f1e094809e832038d459b0f231c8cdbef9e

--- _dist_/index.js
+++ _dist_/index.js
@@ -3,6 +3,7 @@ import { registerImage } from "./lazy.js";
 
 const mountNode = document.getElementById('images');
 const addButton = document.getElementById('addButton');
+const delButton = document.getElementById('delButton');
 
 const max = 123;
 const min = 1;
@@ -15,14 +16,23 @@ const createImageNode = () => {
     const imagen = document.createElement('img');
     imagen.className = 'mx-auto';
     imagen.width = '320';
-    imagen.src = `https://randomfox.ca/images/${random()}.jpg`;
+    imagen.dataset.src = `https://randomfox.ca/images/${random()}.jpg`;
 
-    container.appendChild(imagen);
+    const imageWrapper = document.createElement('div');
+    imageWrapper.className = 'bg-gray-300';
+    imageWrapper.style.minHeight = '100px';
+    imageWrapper.style.display = 'inline-block';
+
+    
+    imageWrapper.appendChild(imagen);
+    container.appendChild(imageWrapper);
+
+    appendedImages++; // esto modifica el script insertado en html para llevar la cuenta, ver index.html
+    printLog();
 
     return container;
 };
 
-const nuevaImagen = createImageNode();
 
 const addImage = () => {
     const newImage = createImageNode(); // aqui si lleva paréntesis
@@ -33,6 +43,12 @@ const addImage = () => {
 addButton.addEventListener('click', addImage); // aqui el addImage va sin el paréntesis
 
 // por qué al llamar funciones hay veces que lleva paréntesis y otras que no?
+
+const delImage = () => {
+    [...mountNode.childNodes].forEach(child => {child.remove();})
+}
+
+delButton.addEventListener('click', delImage);
 
 
 
--- _dist_/lazy.js
+++ _dist_/lazy.js
@@ -2,15 +2,20 @@ const isIntersecting = (entry) => {
     return entry.isIntersecting; // es una propiedad, que devolverá true o false si está visible o no
 }
 
-const accion = (entry) => { // el entry será la imagen
-    const nodo = entry.target; // entry tiene un atributo target que nos da el nodo
-    console.log('holis');
+const loadImage = (entry) => { // el entry será la imagen
+    const container = entry.target; // entry tiene un atributo target que nos da el nodo. Hace referencia al container creado para nuestra imagen (el DIV)
+    const wrapper = container.firstChild;
+    const imagen = wrapper.firstChild;
+    const url = imagen.dataset.src;
+    imagen.src = url;
+    loadedImages++;
+    printLog();
 
-    observer.unobserve(nodo); // una vez cargada no necesitamos observarla así que la desligamos del observer por nodo
+    observer.unobserve(container); // una vez cargada no necesitamos observarla así que la desligamos del observer por nodo
 }
 // intersectionObserver
 const observer = new IntersectionObserver((entries) => {
-    entries.filter(isIntersecting).forEach(accion);
+    entries.filter(isIntersecting).forEach(loadImage); //filtramos entries a las que estén interseccionando y por cada una ejecutamos cargar imagen
 });
 
 
